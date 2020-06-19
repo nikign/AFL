8468dc5c87ee9da5996f311d4e0b9979e022b84e
Niki Gitinabard <nikign@google.com>
2020-06-19 19:50:05 +0000

    added testing for block ids to be in the range of 0 to the total number of blocks

--- Tests/test_counter_BBid_coverage.py
+++ Tests/test_counter_BBid_coverage.py
@@ -24,46 +24,55 @@ def calculate_coverage_for_file(cpp_file, test_input):
     os.remove(object_file)
     os.remove(bc_file)
 
-    line_coverage = {}
+    block_coverage = {}
     with open(coverage_file, 'r') as coverage_logs:
         for l in coverage_logs:
             coverage_info = l.strip().split(':')
             assert len(coverage_info)==2, 'Coverage format file should be block:coverage count'
             block, coverage = int(coverage_info[0]), int(coverage_info[1])
-            line_coverage[block] = coverage
+            block_coverage[block] = coverage
 
     os.remove(get_full_path('test_bb_coverage_out'))
-    return line_coverage
+    return block_coverage
+
+
+def check_block_id_range(block_coverage, total_blocks):
+    for i in block_coverage:
+        assert 0 <= i <= total_blocks, "Block ID should be in the range of 0 to %d" % total_blocks
 
 
 def test_coverage():
     test_program = get_full_path('test_program/test_instr.c')
 
     test_input = get_full_path('test_inputs/example1.txt')
-    line_coverage = calculate_coverage_for_file(test_program, test_input)
-    assert len(line_coverage) < 11, 'The test_instr.c file should only have 10 blocks.'
-    assert line_coverage[9] == 1667, 'Wrong loop coverage. The for loop should be covered 1667 times (5000/3), but is %d times.' % line_coverage[9]
-    assert line_coverage[3] == line_coverage[5] == 7, 'Since the test input has 7 lines, these parts should be covered 7 times, but are covered %d and %d times.' % (line_coverage[3], line_coverage[5])
-    assert 8 not in line_coverage, 'Block 8 is only covered when the test file does not exist.'
+    block_coverage = calculate_coverage_for_file(test_program, test_input)
+    check_block_id_range(block_coverage, 10)
+    assert len(block_coverage) < 11, 'The test_instr.c file should only have 10 blocks.'
+    assert block_coverage[9] == 1667, 'Wrong loop coverage. The for loop should be covered 1667 times (5000/3), but is %d times.' % block_coverage[9]
+    assert block_coverage[3] == block_coverage[5] == 7, 'Since the test input has 7 lines, these parts should be covered 7 times, but are covered %d and %d times.' % (block_coverage[3], block_coverage[5])
+    assert 8 not in block_coverage, 'Block 8 is only covered when the test file does not exist.'
 
     test_input = get_full_path('test_inputs/example2.txt')
-    line_coverage = calculate_coverage_for_file(test_program, test_input)
-    assert len(line_coverage) < 11, 'The test_instr.c file should only have 10 blocks.'
-    assert line_coverage[9] == 1667, 'Wrong loop coverage. The for loop should be covered 1667 times (5000/3), but is %d times.' % line_coverage[9]
-    assert line_coverage[3] == line_coverage[5] == 4, 'Since the test input has 7 lines, these parts should be covered 7 times, but are covered %d and %d times.' % (line_coverage[3], line_coverage[5])
-    assert 8 not in line_coverage, 'Block 8 is only covered when the test file does not exist.'
+    block_coverage = calculate_coverage_for_file(test_program, test_input)
+    check_block_id_range(block_coverage, 10)
+    assert len(block_coverage) < 11, 'The test_instr.c file should only have 10 blocks.'
+    assert block_coverage[9] == 1667, 'Wrong loop coverage. The for loop should be covered 1667 times (5000/3), but is %d times.' % block_coverage[9]
+    assert block_coverage[3] == block_coverage[5] == 4, 'Since the test input has 7 lines, these parts should be covered 7 times, but are covered %d and %d times.' % (block_coverage[3], block_coverage[5])
+    assert 8 not in block_coverage, 'Block 8 is only covered when the test file does not exist.'
 
     test_input = get_full_path('test_inputs/example_not_exists.txt')
-    line_coverage = calculate_coverage_for_file(test_program, test_input)
-    assert len(line_coverage) < 11, 'The test_instr.c file should only have 10 blocks.'
-    assert 8 in line_coverage, 'File does not exist and line 8 should be covered.'
-    assert 3 not in line_coverage, 'File does not exist so the process part of code should not be covered'
+    block_coverage = calculate_coverage_for_file(test_program, test_input)
+    check_block_id_range(block_coverage, 10)
+    assert len(block_coverage) < 11, 'The test_instr.c file should only have 10 blocks.'
+    assert 8 in block_coverage, 'File does not exist and line 8 should be covered.'
+    assert 3 not in block_coverage, 'File does not exist so the process part of code should not be covered'
 
     test_input = get_full_path('test_inputs/example_empty_file.txt')
-    line_coverage = calculate_coverage_for_file(test_program, test_input)
-    assert len(line_coverage) < 11, 'The test_instr.c file should only have 10 blocks.'
-    assert 3 not in line_coverage, 'File is empty, so the process part of code should not be covered'
-    assert 9 in line_coverage, 'Program should continue the part after file handling once the file is empty'
+    block_coverage = calculate_coverage_for_file(test_program, test_input)
+    check_block_id_range(block_coverage, 10)
+    assert len(block_coverage) < 11, 'The test_instr.c file should only have 10 blocks.'
+    assert 3 not in block_coverage, 'File is empty, so the process part of code should not be covered'
+    assert 9 in block_coverage, 'Program should continue the part after file handling once the file is empty'
 
 
 test_coverage()
